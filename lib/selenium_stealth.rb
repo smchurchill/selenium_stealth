bc9e6a25eaf7160496597d1a926e705cfc723af6
Sam Churchill <meadowschurchill@gmail.com>
2024-07-11 16:53:35 -0700

    init commit

---
+++ lib/selenium_stealth.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "selenium_stealth/version"
+
+module SeleniumStealth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/selenium_stealth/version copy.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Selenium
+  module Stealth
+    VERSION = "0.1.0"
+  end
+end
